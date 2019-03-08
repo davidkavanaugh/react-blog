eb8287e3a6fccc274a7da3f79bd039aebf13d43c
davidkavanaugh <davidkavanaugh@outlook.com>
2019-03-08 15:42:31 -0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-import React from 'react';
-import Strapi from "strapi-sdk-javascript/build/main";
+import React, { Component } from 'react';
+import Blog from './Blog/Blog'
+import './App.css';
 
-const strapi = new Strapi("https://strapi-blog-cms.herokuapp.com");
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-   }
- }
+class App extends Component {
 
-async componentDidMount() {
- try {
-   const posts = await strapi.getEntries("blogposts")
-   this.setState({ posts });
- } 
- catch(err) {
-  alert(err);
- }
+  render() {
+    return (
+      <div className="App">
+        <Blog />
+      </div >
+    );
+  }
 }
-render() {
-  return (
-    <section>
-      {this.state.posts.map(post => 
-        <article>
-         <div>Title: {post.title}</div>
-         <div>Content: {post.content}</div>
-        </article>
-      )}
-    </section>
-  )
- }
-}
+
 export default App;
---
+++ src/Blog/Blog.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Strapi from "strapi-sdk-javascript/build/main";
+
+import './Blog.css';
+
+const strapi = new Strapi("https://strapi-blog-cms.herokuapp.com");
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+   }
+ }
+
+async componentDidMount() {
+ try {
+   const posts = await strapi.getEntries("blogposts")
+   this.setState({ posts });
+ } 
+ catch(err) {
+  alert(err);
+ }
+}
+render() {
+  return (
+    <section>
+      {this.state.posts.map(post => 
+        <article className="blog-post" key={post.id}>
+         <div className="blog-post__title">{post.title}</div>
+         <div className="blog-post__author">by {post.author.username}</div>
+         <div className="blog-post__content">{post.content}</div>
+        </article>
+      )}
+    </section>
+  )
+ }
+}
+export default Blog;
